93e0b08842742616ad7dd8445f741441bc351d91
Elmar Ismayilov <bamazy@gmail.com>
2019-02-06 13:18:45 +0400

    cors allow added

--- app/routes/category_routes.js
+++ app/routes/category_routes.js
@@ -1,4 +1,10 @@
 module.exports = function(app, db) {
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     app.get('/categories', (req, res) => {
         db.query('select id, title, slug from category where enabled = 1', function(error, result, fields) {
             if(error) {
