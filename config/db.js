e0b16e0cbbf4b4512c3ec112b3425b3c695ad3ed
Elmar Ismayilov <bamazy@gmail.com>
2022-11-07 18:19:17 +0800

    Formatting applied

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: '144.76.61.53',
-    port: 3306,
-    user: 'temalist',
-    password: 'luR7LkAwyA5u4OyQ8ZSl',
-    database: 'temalist',
-    insecureAuth: true
+  host: "144.76.61.53",
+  port: 3306,
+  user: "temalist",
+  password: "luR7LkAwyA5u4OyQ8ZSl",
+  database: "temalist",
+  insecureAuth: true,
 });
 
 connection.connect();
