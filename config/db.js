58d91f3ca9c91abbab80e566af34ffe0ae6117eb
Elmar Ismayilov <bamazy@gmail.com>
2022-11-07 19:00:31 +0800

    credentials moved to environment variables

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 var mysql = require("mysql");
+var dotenv = require('dotenv')
+dotenv.config();
 
 var connection = mysql.createConnection({
-  host: "***REMOVED***",
-  port: 3306,
-  user: "temalist",
-  password: "password",
-  database: "temalist",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   insecureAuth: true,
 });
 
